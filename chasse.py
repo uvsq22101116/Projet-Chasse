379b5e91d143ab5c66c84e1e86b9eefbfa34cd2d
uvsq22101116 <99487238+uvsq22101116@users.noreply.github.com>
2022-05-04 00:00:20 +0200

    Créer prédateur et correction de créer proies

--- chasse.py
+++ chasse.py
@@ -54,18 +54,37 @@ def init_affichage():
 
 # Création des proies et des prédateurs
 
-def creer_proies(Apro=5, x=15, y=15):
+PROIES=[]
+def creer_proies(Apro= 15, x=15, y=15 ):
     global PROIES 
     a = 0
     if len(PROIES) >= 1:
          a = PROIES[-1][0] + 1
-    PROIES.append(a, Apro, [x,y])
+    PROIES.append [a, Apro, [x, y]]
+
+
 
 def creer_n_proies(Npro=5):
     for i in range(Npro):
         x = rd.randint(0, 29) 
         y = rd.randint(0, 29) 
-        creer_proies(x = x, y = y)
+creer_proies(x = x, y = y)
+
+PREDATEURS= []
+
+def creer_predateurs(Apre=15, Epre=12, cible=[], x=15, y=15):
+    global PREDATEURS
+    a=0
+    if len(PREDATEURS)>=1:
+        a= PREDATEURS[-1][0]+1
+    PREDATEURS.append[a, Apre, Epre, cible, [x, y]]
+    
+
+def creer_n_predateurs(Npre=5):
+    for i in range(Npre):
+        x = rd.randint(0, 29) 
+        y = rd.randint(0, 29) 
+creer_predateurs(x = x, y = y)
 # Mouvements généraux
 
 def simulation():
